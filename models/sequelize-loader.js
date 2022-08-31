a6525995d916591e585925549bd5d6586b687fa8
Tanaka <f.tanaka@gftd.works>
2022-08-31 11:18:56 +0900

    Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,8 +1,32 @@
 'use strict';
 const {Sequelize, DataTypes} = require('sequelize');
-const sequelize = new Sequelize(
-  'postgres://postgres:postgres@db/schedule_arranger'
-);
+
+const dialectOptions = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+};
+
+//本番環境（Heroku）か開発環境かで、接続するデータベースを切り替える
+const sequelize = process.env.DATABASE_URL ?
+
+  //本番環境
+  new Sequelize(
+    process.env.DATABASE_URL,
+    {
+      logging: false,
+      dialectOptions
+    }
+  )
+  :
+  //開発環境
+  new Sequelize(
+    'postgres://postgres:postgres@db/schedule_arranger',
+    {
+      logging: false
+    }
+  );
 
 module.exports = {
   sequelize,
